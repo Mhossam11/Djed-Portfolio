dd00e37c80da37114d0ab6d3c4f010ecb6956683
Mhossam11 <mhossoam572000@gmail.com>
2025-09-04 06:35:33 +0300

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from 'next/font/google';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Gym-Z - Unifying Fitness Management',
+  title: 'DjedFit',
   description: 'A comprehensive software solution for nutrition, supplements, and exercise management between coaches and clients.',
 };
 
